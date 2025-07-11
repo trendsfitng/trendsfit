3ee65471e8764de31dcc24bb920cd11473a3cbc8
tonnipaul <ariyoadeoyepaul@gmail.com>
2025-07-11 13:13:11 +0100

    stye: improve adverts styling

--- components/empty-advert.tsx
+++ components/empty-advert.tsx
@@ -3,21 +3,19 @@ import Link from 'next/link';
 
 const EmptyAdverts = () => {
   return (
-    <section className="">
-      <div className="flex flex-col items-center justify-center text-center p-7  bg-white border border-dashed border-gray-300 rounded-xl">
-        <h2 className="text-2xl font-bold">Advertise with us</h2>
-        <p className=" mt-2">
-          Want to advertise with us? Reach thousands of readers daily by placing
-          your ad here.
-        </p>
-        <Link
-          href={routes.contact()}
-          className="mt-4 inline-block px-5 py-2 bg-primary text-white rounded hover:bg-opacity-90 transition"
-        >
-          Contact Us
-        </Link>
-      </div>
-    </section>
+    <div className="flex flex-col items-center justify-center text-center p-7  bg-white border border-dashed border-gray-300 rounded-xl shadow-xl">
+      <h2 className="text-2xl font-bold">Advertise with us</h2>
+      <p className=" mt-2">
+        Want to advertise with us? Reach thousands of readers daily by placing
+        your ad here.
+      </p>
+      <Link
+        href={routes.contact()}
+        className="mt-4 inline-block px-5 py-2 bg-primary text-white rounded hover:bg-opacity-90 transition"
+      >
+        Contact Us
+      </Link>
+    </div>
   );
 };
 
